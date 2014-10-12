03b09a540f055b506f47d2ff1f916d08483ce3d9
Tian Pan <puncsky@gmail.com>
2014-10-11 20:08:41 -0700

    Add initial Mobile.iOS project with basic tab view. #38

---
+++ Mobile.iOS/Main.cs
@@ -0,0 +1,17 @@
+﻿// (c) 2012-2014 Tian Pan (www.puncsky.com). All Rights Reserved.
+
+using UIKit;
+
+namespace Mobile.iOS
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main (string [] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main (args, null, "AppDelegate");
+        }
+    }
+}
